1435801bb2f8b229e29a8939642f35eb6f82a097
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-18 17:51:59 +0530

    Create binary search.py

---
+++ simple algorithms/binary search.py
@@ -0,0 +1,14 @@
+def binary_search(arr, x):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+numbers = [10, 20, 30, 40, 50]
+print(binary_search(numbers, 30))
