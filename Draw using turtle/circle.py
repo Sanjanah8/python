b19076b3cda60cbcdc71f7dd9b7024dcb5f144c0
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-31 22:01:18 +0530

    Create circle.py

---
+++ Draw using turtle/circle.py
@@ -0,0 +1,12 @@
+import turtle
+
+# Set up the screen and turtle
+screen = turtle.Screen()
+screen.title("Draw a Circle")
+t = turtle.Turtle()
+
+# Draw a circle
+t.circle(100)  # Radius of 100 units
+
+# Complete drawing
+turtle.done()
