69f1b162f91471d3ecd7759a7143ab403a7e4de5
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-31 22:25:45 +0530

    Create manim.py

---
+++ libraries/manim.py
@@ -0,0 +1,9 @@
+from manim import *
+
+class SquareToCircle(Scene):
+    def construct(self):
+        square = Square()
+        circle = Circle()
+        self.play(Create(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
